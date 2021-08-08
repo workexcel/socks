bb908735b3128a4f9670cc35512551c3a812b309
hananelgayati <88630892+hananelgayati@users.noreply.github.com>
2021-08-08 20:06:45 +0100

    Create socks.sh

---
+++ socks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "Please enter your pass: "
+read pass
+sudo yum install wget -y
+wget https://raw.githubusercontent.com/hananelgayati/asfcjas/main/dante.sh
+chmod 777 dante.sh
+sudo ./dante.sh
+sudo /etc/init.d/sockd adduser $pass $pass
+echo 'net.ipv4.icmp_echo_ignore_all = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+echo 'Port 22001' | sudo tee -a  /etc/ssh/sshd_config
+sudo systemctl restart sshd.service

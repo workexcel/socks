644be2efb2ca7159080c9e7a91ba012aaef617dd
workexcel <132607030+workexcel@users.noreply.github.com>
2023-05-04 21:09:02 +0300

    Update socks.sh

--- socks.sh
+++ socks.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-echo -e "Please enter your pass: "
-read pass
 sudo yum install wget -y
-wget https://raw.githubusercontent.com/hananelgayati/asfcjas/main/dante.sh
+wget https://raw.githubusercontent.com/workexcel/socks/main/dante.sh
 chmod 777 dante.sh
 sudo ./dante.sh
-sudo /etc/init.d/sockd adduser $pass $pass
+sudo /etc/init.d/sockd adduser www www
 echo 'net.ipv4.icmp_echo_ignore_all = 1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 echo 'Port 22001' | sudo tee -a  /etc/ssh/sshd_config
